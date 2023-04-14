95a5ffc5fe518b4e929cf6c06778ada2c90ceebf
Johann Muszynski <johann.muszynski@gmail.com>
2023-04-14 21:04:15 +0300

    Move main() to the top of the file

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,194 @@ use winit::{
 
 mod raytracer;
 
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("weekend-raytracer-wgpu")
+        .with_inner_size(winit::dpi::PhysicalSize::new(800, 600))
+        .build(&event_loop)
+        .unwrap();
+    let mut context = pollster::block_on(GpuContext::new(&window));
+
+    let viewport_size = {
+        let viewport = window.inner_size();
+        (viewport.width, viewport.height)
+    };
+    let max_viewport_resolution = window
+        .available_monitors()
+        .map(|monitor| -> u32 {
+            let size = monitor.size();
+            size.width * size.height
+        })
+        .max()
+        .expect("There should be at least one monitor available");
+
+    let scene = scene();
+    let mut fly_camera_control_state = FlyCameraControlState::default();
+    let mut fly_camera = FlyCamera::default();
+
+    let mut render_params = RenderParams {
+        camera: fly_camera.renderer_camera(),
+        viewport_size,
+    };
+    let mut raytracer = Raytracer::new(
+        &context.device,
+        &context.surface_config,
+        scene,
+        render_params,
+        max_viewport_resolution,
+    );
+
+    let mut last_time = Instant::now();
+
+    event_loop.run(move |event, _, _control_flow| match event {
+        Event::NewEvents(_) => {
+            fly_camera_control_state.begin_frame();
+        }
+
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::CloseRequested => {
+                *_control_flow = ControlFlow::Exit;
+            }
+
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state,
+                        virtual_keycode: Some(keycode),
+                        ..
+                    },
+                ..
+            } => {
+                let is_pressed = state == ElementState::Pressed;
+                match keycode {
+                    VirtualKeyCode::W => {
+                        fly_camera_control_state.forward_pressed = is_pressed;
+                    }
+                    VirtualKeyCode::S => {
+                        fly_camera_control_state.backward_pressed = is_pressed;
+                    }
+                    VirtualKeyCode::A => {
+                        fly_camera_control_state.left_pressed = is_pressed;
+                    }
+                    VirtualKeyCode::D => {
+                        fly_camera_control_state.right_pressed = is_pressed;
+                    }
+                    VirtualKeyCode::Q => {
+                        fly_camera_control_state.down_pressed = is_pressed;
+                    }
+                    VirtualKeyCode::E => {
+                        fly_camera_control_state.up_pressed = is_pressed;
+                    }
+                    _ => {}
+                }
+            }
+
+            WindowEvent::CursorMoved { position, .. } => {
+                let position = (position.x as f32, position.y as f32);
+                if let Some(previous_position) = fly_camera_control_state.previous_mouse_pos {
+                    fly_camera_control_state.mouse_delta = (
+                        position.0 - previous_position.0,
+                        previous_position.1 - position.1,
+                    );
+                }
+                fly_camera_control_state.previous_mouse_pos = Some(position);
+            }
+
+            WindowEvent::MouseInput { button, state, .. } => match button {
+                MouseButton::Right => {
+                    fly_camera_control_state.look_pressed = state == ElementState::Pressed;
+                }
+                _ => (),
+            },
+
+            WindowEvent::Resized(physical_size) => {
+                if physical_size.width > 0 && physical_size.height > 0 {
+                    render_params.viewport_size = (physical_size.width, physical_size.height);
+                    context.surface_config.width = physical_size.width;
+                    context.surface_config.height = physical_size.height;
+                    context
+                        .surface
+                        .configure(&context.device, &context.surface_config);
+                }
+            }
+
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                if new_inner_size.width > 0 && new_inner_size.height > 0 {
+                    render_params.viewport_size = (new_inner_size.width, new_inner_size.height);
+                    context.surface_config.width = new_inner_size.width;
+                    context.surface_config.height = new_inner_size.height;
+                    context
+                        .surface
+                        .configure(&context.device, &context.surface_config);
+                }
+            }
+
+            _ => {}
+        },
+
+        Event::MainEventsCleared => {
+            {
+                let dt = last_time.elapsed().as_secs_f32();
+                last_time = Instant::now();
+
+                fly_camera.translate(fly_camera_control_state.translation(2.0 * dt));
+                fly_camera.rotate(fly_camera_control_state.rotation(0.5 * dt));
+            }
+
+            render_params.camera = fly_camera.renderer_camera();
+            raytracer.set_render_params(&context.queue, render_params);
+
+            window.request_redraw();
+        }
+
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            let frame = match context.surface.get_current_texture() {
+                Ok(frame) => frame,
+                Err(e) => {
+                    eprintln!("Surface error: {:?}", e);
+                    return;
+                }
+            };
+
+            let view = frame
+                .texture
+                .create_view(&wgpu::TextureViewDescriptor::default());
+
+            let mut encoder = context
+                .device
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+            {
+                let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(wgpu::Color {
+                                r: 0.012,
+                                g: 0.012,
+                                b: 0.012,
+                                a: 1.0,
+                            }),
+                            store: true,
+                        },
+                    })],
+                    depth_stencil_attachment: None,
+                    label: None,
+                });
+
+                raytracer.render_frame(&mut render_pass);
+            }
+
+            context.queue.submit(Some(encoder.finish()));
+            frame.present();
+        }
+
+        _ => {}
+    });
+}
+
 struct GpuContext {
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -238,192 +426,4 @@ fn scene() -> Scene {
     ];
 
     Scene { spheres }
-}
-
-fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("weekend-raytracer-wgpu")
-        .with_inner_size(winit::dpi::PhysicalSize::new(800, 600))
-        .build(&event_loop)
-        .unwrap();
-    let mut context = pollster::block_on(GpuContext::new(&window));
-
-    let viewport_size = {
-        let viewport = window.inner_size();
-        (viewport.width, viewport.height)
-    };
-    let max_viewport_resolution = window
-        .available_monitors()
-        .map(|monitor| -> u32 {
-            let size = monitor.size();
-            size.width * size.height
-        })
-        .max()
-        .expect("There should be at least one monitor available");
-
-    let scene = scene();
-    let mut fly_camera_control_state = FlyCameraControlState::default();
-    let mut fly_camera = FlyCamera::default();
-
-    let mut render_params = RenderParams {
-        camera: fly_camera.renderer_camera(),
-        viewport_size,
-    };
-    let mut raytracer = Raytracer::new(
-        &context.device,
-        &context.surface_config,
-        scene,
-        render_params,
-        max_viewport_resolution,
-    );
-
-    let mut last_time = Instant::now();
-
-    event_loop.run(move |event, _, _control_flow| match event {
-        Event::NewEvents(_) => {
-            fly_camera_control_state.begin_frame();
-        }
-
-        Event::WindowEvent { event, .. } => match event {
-            WindowEvent::CloseRequested => {
-                *_control_flow = ControlFlow::Exit;
-            }
-
-            WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state,
-                        virtual_keycode: Some(keycode),
-                        ..
-                    },
-                ..
-            } => {
-                let is_pressed = state == ElementState::Pressed;
-                match keycode {
-                    VirtualKeyCode::W => {
-                        fly_camera_control_state.forward_pressed = is_pressed;
-                    }
-                    VirtualKeyCode::S => {
-                        fly_camera_control_state.backward_pressed = is_pressed;
-                    }
-                    VirtualKeyCode::A => {
-                        fly_camera_control_state.left_pressed = is_pressed;
-                    }
-                    VirtualKeyCode::D => {
-                        fly_camera_control_state.right_pressed = is_pressed;
-                    }
-                    VirtualKeyCode::Q => {
-                        fly_camera_control_state.down_pressed = is_pressed;
-                    }
-                    VirtualKeyCode::E => {
-                        fly_camera_control_state.up_pressed = is_pressed;
-                    }
-                    _ => {}
-                }
-            }
-
-            WindowEvent::CursorMoved { position, .. } => {
-                let position = (position.x as f32, position.y as f32);
-                if let Some(previous_position) = fly_camera_control_state.previous_mouse_pos {
-                    fly_camera_control_state.mouse_delta = (
-                        position.0 - previous_position.0,
-                        previous_position.1 - position.1,
-                    );
-                }
-                fly_camera_control_state.previous_mouse_pos = Some(position);
-            }
-
-            WindowEvent::MouseInput { button, state, .. } => match button {
-                MouseButton::Right => {
-                    fly_camera_control_state.look_pressed = state == ElementState::Pressed;
-                }
-                _ => (),
-            },
-
-            WindowEvent::Resized(physical_size) => {
-                if physical_size.width > 0 && physical_size.height > 0 {
-                    render_params.viewport_size = (physical_size.width, physical_size.height);
-                    context.surface_config.width = physical_size.width;
-                    context.surface_config.height = physical_size.height;
-                    context
-                        .surface
-                        .configure(&context.device, &context.surface_config);
-                }
-            }
-
-            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                if new_inner_size.width > 0 && new_inner_size.height > 0 {
-                    render_params.viewport_size = (new_inner_size.width, new_inner_size.height);
-                    context.surface_config.width = new_inner_size.width;
-                    context.surface_config.height = new_inner_size.height;
-                    context
-                        .surface
-                        .configure(&context.device, &context.surface_config);
-                }
-            }
-
-            _ => {}
-        },
-
-        Event::MainEventsCleared => {
-            {
-                let dt = last_time.elapsed().as_secs_f32();
-                last_time = Instant::now();
-
-                fly_camera.translate(fly_camera_control_state.translation(2.0 * dt));
-                fly_camera.rotate(fly_camera_control_state.rotation(0.5 * dt));
-            }
-
-            render_params.camera = fly_camera.renderer_camera();
-            raytracer.set_render_params(&context.queue, render_params);
-
-            window.request_redraw();
-        }
-
-        Event::RedrawRequested(window_id) if window_id == window.id() => {
-            let frame = match context.surface.get_current_texture() {
-                Ok(frame) => frame,
-                Err(e) => {
-                    eprintln!("Surface error: {:?}", e);
-                    return;
-                }
-            };
-
-            let view = frame
-                .texture
-                .create_view(&wgpu::TextureViewDescriptor::default());
-
-            let mut encoder = context
-                .device
-                .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-
-            {
-                let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                        view: &view,
-                        resolve_target: None,
-                        ops: wgpu::Operations {
-                            load: wgpu::LoadOp::Clear(wgpu::Color {
-                                r: 0.012,
-                                g: 0.012,
-                                b: 0.012,
-                                a: 1.0,
-                            }),
-                            store: true,
-                        },
-                    })],
-                    depth_stencil_attachment: None,
-                    label: None,
-                });
-
-                raytracer.render_frame(&mut render_pass);
-            }
-
-            context.queue.submit(Some(encoder.finish()));
-            frame.present();
-        }
-
-        _ => {}
-    });
 }
